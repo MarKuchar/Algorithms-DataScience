9d96aa2dc2ec2934ae3aeb2bc0ca5b673e077149
Martin Kuchar <kucharmartin0@gmail.com>
2020-06-23 16:00:51 -0700

    Init new command line project

---
+++ AGDS/SwiftAlgorithmAndDataStructure/SwiftAlgorithmAndDataStructure/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAlgorithmAndDataStructure
+//
+//  Created by Martin Kuchar on 2020-06-23.
+//  Copyright © 2020 Martin Kuchar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
