d96bab48fc5cf947f9c48af8ec4eb5a20d95e9b2
Martin Kuchar <kucharmartin0@gmail.com>
2020-06-24 08:28:07 -0700

    Fix some

--- AGDS/SwiftAlgorithmAndDataStructure/SwiftAlgorithmAndDataStructure/Recursion/Recursion.swift
+++ AGDS/SwiftAlgorithmAndDataStructure/SwiftAlgorithmAndDataStructure/Recursion/Recursion.swift
@@ -18,18 +18,21 @@ func reverseLines (_ line: Int) {
     print("\(lines[line])")
 }
 
-func operatorFunction (_ leftValue: Int, _ rightValue: Int, _ oper: String) -> Int? {
-    if oper == "+" {
-        return leftValue + rightValue
-    } else if oper == "-" {
-        return leftValue - rightValue
-    } else if oper == "/" {
-        return leftValue / rightValue
-    } else if oper == "*" {
-        return leftValue * rightValue
-    } else {
-        return nil
+func operatorFunction (_ leftValue: Int?, _ rightValue: Int?, _ oper: String) -> Int? {
+    if let rightValue = rightValue, let leftValue = leftValue {
+        if oper == "+" {
+            return leftValue + rightValue
+        } else if oper == "-" {
+            return leftValue - rightValue
+        } else if oper == "/" {
+            return leftValue / rightValue
+        } else if oper == "*" {
+            return leftValue * rightValue
+        } else {
+            return nil
+        }
     }
+    return nil
 }
 
 func indexHelper(_ expression: String) -> String {
@@ -38,37 +41,25 @@ func indexHelper(_ expression: String) -> String {
 
 func evaluateExpression (_ expression: String) -> Int {
 //    let operators = CharacterSet.init(charactersIn: "+-/*")
+    if expression.count == 0 {
+        return 0
+    }
     if let value = Int(expression) {
         return value
     } else if expression[0] == "(" {
-        if let indexOpenning = expression[2, expression.count].firstIndex(of: "("), let indexClosing = expression.firstIndex(of: ")") {
+        if let indexOpenning = expression[1, expression.count].firstIndex(of: "("), let indexClosing = expression.firstIndex(of: ")") {
             if indexOpenning < indexClosing {
-                
-                let substringInner = expression[indexOpenning...indexClosing]
-                let evalStringInner = String(substringInner)
                 
                 let substringOuter = expression[...indexOpenning]
                 let evalStringOuter = String(substringOuter)
                 
-                let partRight = evaluateExpression(evalStringInner[0, evalStringInner.count-1])
                 let partLeft = evaluateExpression(evalStringOuter[1, evalStringOuter.count])
-                print(evalStringInner[1, evalStringInner.count-1])
-                print(expression[evalStringOuter.count])
-                if let value = operatorFunction(partLeft, partRight, expression[evalStringOuter.count]) {
-                    print(value)
+                if let value = operatorFunction(partLeft, evaluateExpression(expression[1, expression.count]), expression[2]) {
                     return value
                 }
             }
-        } else if let indexClosing = expression.firstIndex(of: ")") {
-            let substring = expression[...indexClosing]
-            let evalString = String(substring)
-            print(evalString[1, evalString.count-1])
-            print(expression[evalString.count+1, expression.count])
-            print(expression[evalString.count])
-            let part = evaluateExpression(evalString[1, evalString.count-1])
-            if let value = operatorFunction(part, evaluateExpression(expression[evalString.count+1, expression.count]), expression[evalString.count]) {
-                return value
-            }
+        } else if let value = operatorFunction(evaluateExpression(expression[1, 4]), evaluateExpression(expression[6, expression.count]), expression[5]) {
+            return value
         }
         return evaluateExpression(expression[1, expression.count])
     } else if let valueLeft = Int(expression[0]) {
--- AGDS/SwiftAlgorithmAndDataStructure/SwiftAlgorithmAndDataStructure/main.swift
+++ AGDS/SwiftAlgorithmAndDataStructure/SwiftAlgorithmAndDataStructure/main.swift
@@ -9,5 +9,9 @@
 import Foundation
 
 //reverseLines(0)
-print(evaluateExpression("2+(2*1)+2"))
-
+//print(evaluateExpression("8+(3*1)+2)+2"))
+//print(evaluateExpression("(8+3)*(1+2)"))
+//print(evaluateExpression("8+2+2"))
+//print(evaluateExpression("8"))
+//print(evaluateExpression(""))
+//print(evaluateExpression("s"))
