ff5b2b8ad31d315ea5c5d0b74e74e90713d1441e
Martin Kuchar <kucharmartin0@gmail.com>
2020-07-07 16:02:40 -0700

    Use a reverse loop to iterate an array

--- AGDS/SwiftAlgorithmAndDataStructure/Sorting/main.swift
+++ AGDS/SwiftAlgorithmAndDataStructure/Sorting/main.swift
@@ -9,28 +9,29 @@
 import Foundation
 
 
-var arr: [Int] = [3, 2, 5, 1, 4]
+var arr: [Int] = [0, 3, 2, 5, 1, 4, 8]
 
-quickSort(&arr, 0, arr.count-1, >)
+quickSort(&arr, 0, arr.count, >)
 print(arr)
 
 /// Quick sort algorithm
 
 func quickSort<T: Comparable> (_ arr: inout [T], _ startIndex: Int, _ endIndex: Int, _ comparator: (T, T) -> Bool)  {
-    guard endIndex - startIndex <= 1 else {
+    guard endIndex - startIndex > 1 else {
         return
     }
-    quickSort(&arr, startIndex, partition(&arr, startIndex, endIndex, comparator), comparator)
-    quickSort(&arr, partition(&arr, startIndex, endIndex, comparator), endIndex, comparator)
+    let pivotIndex = partition(&arr, startIndex, endIndex, comparator)
+    quickSort(&arr, startIndex, pivotIndex, comparator)
+    quickSort(&arr, pivotIndex, endIndex, comparator)
 }
 
 func partition<T: Comparable> (_ arr: inout [T], _ startIndex: Int, _ endIndex: Int, _ comparator: (T, T) -> Bool)  -> Int {
-    let pivot = arr[endIndex]
-    var newPivotIndex = endIndex
+    let pivot = arr[endIndex-1]
+    var newPivotIndex = endIndex-1
     
-    for i in 0..<arr.count-1 {
+    for i in (startIndex..<newPivotIndex).reversed() {
         if comparator(arr[i], pivot) {
-            arr.append(arr.remove(at: i))
+            arr.insert(arr.remove(at: i), at: newPivotIndex)
             newPivotIndex -= 1
         }
     }
