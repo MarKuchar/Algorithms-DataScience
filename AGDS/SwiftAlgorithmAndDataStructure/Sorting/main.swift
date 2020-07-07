8054a1b3025c3e58aa5b51fe9943b7ccab425e33
Martin Kuchar <kucharmartin0@gmail.com>
2020-07-07 15:12:43 -0700

    Write the first logic of quick the sort alg.

--- AGDS/SwiftAlgorithmAndDataStructure/Sorting/main.swift
+++ AGDS/SwiftAlgorithmAndDataStructure/Sorting/main.swift
@@ -8,13 +8,32 @@
 
 import Foundation
 
+
+var arr: [Int] = [3, 2, 5, 1, 4]
+
+quickSort(&arr, 0, arr.count-1, >)
+print(arr)
+
 /// Quick sort algorithm
 
 func quickSort<T: Comparable> (_ arr: inout [T], _ startIndex: Int, _ endIndex: Int, _ comparator: (T, T) -> Bool)  {
-    
+    guard endIndex - startIndex <= 1 else {
+        return
+    }
+    quickSort(&arr, startIndex, partition(&arr, startIndex, endIndex, comparator), comparator)
+    quickSort(&arr, partition(&arr, startIndex, endIndex, comparator), endIndex, comparator)
 }
 
 func partition<T: Comparable> (_ arr: inout [T], _ startIndex: Int, _ endIndex: Int, _ comparator: (T, T) -> Bool)  -> Int {
-    return 0
+    let pivot = arr[endIndex]
+    var newPivotIndex = endIndex
+    
+    for i in 0..<arr.count-1 {
+        if comparator(arr[i], pivot) {
+            arr.append(arr.remove(at: i))
+            newPivotIndex -= 1
+        }
+    }
+    return newPivotIndex
 }
 
