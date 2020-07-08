1cf5fc0d32fe790185854ce068ea2df2b2e583e5
Martin Kuchar <kucharmartin0@gmail.com>
2020-07-07 17:31:48 -0700

    Import NQueensPrblm files

---
+++ AGDS/SwiftAlgorithmAndDataStructure/NQueensPrblm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  NQueenPrblm
+//
+//  Created by Martin Kuchar on 2020-07-07.
+//  Copyright © 2020 Martin Kuchar. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
