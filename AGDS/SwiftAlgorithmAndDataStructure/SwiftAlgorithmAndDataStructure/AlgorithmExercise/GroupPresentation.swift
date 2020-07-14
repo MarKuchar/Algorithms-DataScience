cc876e9f5aaa47e7a596ed79672b74b7ee1ec71e
Martin Kuchar <kucharmartin0@gmail.com>
2020-07-13 17:28:02 -0700

    Init algorithm practice

---
+++ AGDS/SwiftAlgorithmAndDataStructure/SwiftAlgorithmAndDataStructure/AlgorithmExercise/GroupPresentation.swift
@@ -0,0 +1,14 @@
+//
+//  GroupPresentation.swift
+//  SwiftAlgorithmAndDataStructure
+//
+//  Created by Martin Kuchar on 2020-07-13.
+//  Copyright © 2020 Martin Kuchar. All rights reserved.
+//
+
+import Foundation
+
+func findElements() -> [Int] {
+    
+    return [1]
+}
