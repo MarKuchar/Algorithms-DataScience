0f82199b88c2677845f07e0ca99ef728e13f2545
Martin Kuchar <kucharmartin0@gmail.com>
2020-07-18 04:19:24 -0700

    Finish first part of quiz

--- AGDS/SwiftAlgorithmAndDataStructure/Trees/TreeQuiz.swift
+++ AGDS/SwiftAlgorithmAndDataStructure/Trees/TreeQuiz.swift
@@ -11,13 +11,20 @@ import Foundation
 
 func printParent() {
     let numberOfNodes = Int(readLine()!)!
+    var hasParent = [Bool](repeating: false, count: numberOfNodes)
+    hasParent[0] = true
     var parents = [Int](repeating: 0, count: numberOfNodes+1)
     for _ in 0..<numberOfNodes-1 {
         let edge = readLine()!.split(separator: " ")
         let nodeA = Int(edge[0])!
         let nodeB = Int(edge[1])!
-        
-        parents[nodeB] = nodeA
+        if hasParent[nodeA-1] {
+            parents[nodeB] = nodeA
+            hasParent[nodeB-1] = true
+        } else {
+            parents[nodeA] = nodeB
+            hasParent[nodeA-1] = true
+        }
     }
     for i in 2...numberOfNodes {
         print(parents[i])
