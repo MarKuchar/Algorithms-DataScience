55b7d937b0946ab196829d208b37f3355a8a48b8
Martin Kuchar <kucharmartin0@gmail.com>
2020-07-20 15:48:58 -0700

    Use struct to hold the info about vertices

--- AGDS/SwiftAlgorithmAndDataStructure/Trees/LowestCommonAncestor.swift
+++ AGDS/SwiftAlgorithmAndDataStructure/Trees/LowestCommonAncestor.swift
@@ -8,4 +8,31 @@
 
 import Foundation
 
+struct Vertex {
+    var parent: Int?
+    var children: [Int]?
+}
 
+func lowestCommonAncestor() {
+    let numOfVertices = Int(readLine()!)!
+    var treeVertices = [Vertex](repeating: Vertex(parent: nil, children: []), count: numOfVertices)
+    treeVertices[0].parent = 0
+    
+    for _ in 0..<numOfVertices-1 {
+        let edge = readLine()!.split(separator: " ")
+        let vertexA = Int(edge[0])
+        let vertexB = Int(edge[1])
+        if let a = vertexA, let b = vertexB {
+            if treeVertices[a-1].parent != nil {
+                treeVertices[b-1].parent = vertexA
+                treeVertices[a-1].children?.append(b)
+            } else {
+                treeVertices[a-1].parent = vertexB
+                treeVertices[b-1].children?.append(a)
+            }
+        }
+    }
+    for i in treeVertices {
+     print(i)
+    }
+}
--- AGDS/SwiftAlgorithmAndDataStructure/Trees/main.swift
+++ AGDS/SwiftAlgorithmAndDataStructure/Trees/main.swift
@@ -9,5 +9,8 @@
 import Foundation
 
 //printParent()
-diameter()
+//diameter()
+
+lowestCommonAncestor()
+
 
