b72944ffdc93fb5502a36428d470a22645329586
Martin Kuchar <kucharmartin0@gmail.com>
2020-07-21 19:30:35 -0700

    Finish findCycle function using DFS for disconnected graphs

--- AGDS/SwiftAlgorithmAndDataStructure/Graphs/Assignments/Permutation.swift
+++ AGDS/SwiftAlgorithmAndDataStructure/Graphs/Assignments/Permutation.swift
@@ -11,13 +11,39 @@ import Foundation
 
 func findCycle() {
     let numberOfTests = Int(readLine()!)!
-    let permutationSize = Int(readLine()!)!
-    let permutationSet = readLine()!.split(separator: " ").map { Int($0)! }
-    var adjList = [[Int]](repeating: [], count: permutationSize+1)
-    
-    for i in 1...numberOfTests {
-        adjList[i].append(permutationSet[i-1])
+
+    for _ in 0..<numberOfTests {
+        let permutationSize = Int(readLine()!)!
+        let permutationSet = readLine()!.split(separator: " ").map { Int($0)! }
+        var adjList = [[Int]](repeating: [], count: permutationSize+1)
+        for i in 1...permutationSize {
+            adjList[i].append(permutationSet[i-1])
+        }
         
+        // DFS
+        var counter = 0
+        func dfs(_ currentVertex: Int, _ isVisited: inout [Bool], _ adjList: inout [[Int]], _ counter: inout Int, _ inLoopTrack: inout [Bool]) {
+            isVisited[currentVertex] = true
+            inLoopTrack[currentVertex] = true
+            for v in adjList[currentVertex] {
+                if inLoopTrack[v] {
+                    counter += 1
+                }
+                if !isVisited[v] {
+                    dfs(v, &isVisited, &adjList, &counter, &inLoopTrack)
+                }
+            }
+        }
+        
+        // Find loops
+        var isVisited = [Bool](repeating: false, count: permutationSize+1)
+        var inLoopTrack = [Bool](repeating: false, count: permutationSize+1)
+        
+        for i in 1...permutationSize {
+            if !isVisited[i] {
+                dfs(i, &isVisited, &adjList, &counter, &inLoopTrack)
+            }
+        }
+        print(counter)
     }
-    
 }
