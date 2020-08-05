aced0f2b0d322bb6fdc74e38f09ef274b8d99a08
Martin Kuchar <kucharmartin0@gmail.com>
2020-08-04 20:48:55 -0700

    Use prim's algorithm using indexPQ to find MST

--- AGDS/SwiftAlgorithmAndDataStructure/Trees/MinimumCostFlow.swift
+++ AGDS/SwiftAlgorithmAndDataStructure/Trees/MinimumCostFlow.swift
@@ -8,10 +8,14 @@
 
 import Foundation
 
-struct Pipe {
+struct Pipe: Comparable, Hashable {
     let to: Int
     let cost: Int
-    var isActive: Bool
+    var isActive = false
+    
+    static func < (lhs: Pipe, rhs: Pipe) -> Bool {
+        return lhs.cost < rhs.cost
+    }
 }
 
 func minimumCostFlow() {
@@ -20,13 +24,41 @@ func minimumCostFlow() {
     let m = firstLine[1]
     let d = firstLine[2]
     
-    var adjList = [[Pipe]]()
+    var pipeArr = [[Pipe]](repeating: [], count: n + 1)
+    var pipeQueue = IndexPriorityQueue<Pipe>(>)
     
     for i in 0..<m {
-        let pipe = readLine()!.split(separator: " ").map { Int($0)! }
-        
-        if i < n {
-            
+        let line = readLine()!.split(separator: " ").map { Int($0)! }
+        var pipe = Pipe(to: line[1], cost: line[2])
+        var pipeB = Pipe(to: line[0], cost: line[2])
+        if i < n - 1 {
+            pipe.isActive = true
+            pipeB.isActive = true
+        }
+        pipeArr[line[0]].append(pipe)
+        pipeArr[line[1]].append(pipeB)
+    }
+    
+    var isConnected = [Bool](repeating: false, count: n + 1)
+    pipeQueue.enqueue(Pipe(to: 1, cost: 0, isActive: true))
+    var days = 0
+    var connected = 0
+    var totalCost = 0
+    
+    while connected < n {
+        let b = pipeQueue.dequeue()!
+        if !b.isActive {
+            days += 1
+        }
+        totalCost += b.cost
+        connected += 1
+        isConnected[b.to] = true
+        for i in pipeArr[b.to] {
+            if !isConnected[i.to] {
+                pipeQueue.enqueue(i)
+            }
         }
     }
+    print(totalCost)
+    print(days)
 }
