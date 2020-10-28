d9abacd5318c7d432074a4d3f9c7619e4566603f
Martin Kuchar <kucharmartin0@gmail.com>
2020-10-27 18:40:05 -0700

    Pracitce tree traversal

---
+++ AGDS/SwiftAlgorithmAndDataStructure/Trees/Practice/Practice.swift
@@ -0,0 +1,21 @@
+//
+//  Practice.swift
+//  SwiftAlgorithmAndDataStructure
+//
+//  Created by Martin Kuchar on 2020-10-27.
+//  Copyright © 2020 Martin Kuchar. All rights reserved.
+//
+
+import Foundation
+
+func findParent() {
+    let numOfNodes = Int(readLine()!)!
+    var parrents = [Int](repeating: -1, count: numOfNodes)
+    var adjList =[[Int]](repeating: [], count: numOfNodes)
+    for i in 0..<numOfNodes {
+        let line = readLine()!.split(separator: " ")
+        let a = Int(line[0])!
+        let b = Int(line[1])!
+        adjList
+    }
+}
